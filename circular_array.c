d11270f4374383bb9bbdd44210775146e5e4106d
MatheusSRMO <matheus.souzaribeiro@gmail.com>
2023-06-06 16:02:14 -0300

    feat: implementing circular_array_construct

--- circular_array.c
+++ circular_array.c
@@ -9,3 +9,16 @@ struct circular_array {
     int rear;
     bool dynamic;
 };
+
+CircularArray* circular_array_construct(int capacity, bool dynamic) {
+    CircularArray* circular_array = malloc(sizeof(CircularArray));
+
+    circular_array->capacity = capacity;
+    circular_array->size = 0;
+    circular_array->front = 0;
+    circular_array->rear = -1;
+    circular_array->dynamic = dynamic;
+    circular_array->items = malloc(sizeof(data_type) * capacity);
+
+    return circular_array;
+}
