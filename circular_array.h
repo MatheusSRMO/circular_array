33954f1504c860acd51341629b4ab603ff33d5cf
MatheusSRMO <matheus.souzaribeiro@gmail.com>
2023-06-06 15:30:49 -0300

    feat: first commit

---
+++ circular_array.h
@@ -0,0 +1,21 @@
+#ifndef CIRCULAR_ARRAY_H
+#define CIRCULAR_ARRAY_H
+
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+
+typedef int data_type;
+typedef struct circular_array CircularArray;
+
+
+CircularArray* circular_array_construct(int capacity, bool dynamic);
+bool circular_array_is_empty(CircularArray* circular_array);
+bool circular_array_is_full(CircularArray* circular_array);
+void circular_array_push(CircularArray* circular_array, data_type data);
+data_type circular_array_pop(CircularArray* circular_array);
+void circular_array_destruct(CircularArray* circular_array);
+
+#endif // CIRCULAR_ARRAY_H
---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include "circular_array.h"
+
+int main() {
+
+    return 0;
+}
