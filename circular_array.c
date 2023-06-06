c6b8ea5d21d7c6b3e90d702b1484c362290c0bde
MatheusSRMO <matheus.souzaribeiro@gmail.com>
2023-06-06 16:23:37 -0300

    feat: implementing circular_array_push

--- circular_array.c
+++ circular_array.c
@@ -47,3 +47,38 @@ bool circular_array_is_full(CircularArray* circular_array) {
         return false;
     }
 }
+
+void circular_array_push(CircularArray* circular_array, data_type data) {
+    if (circular_array_is_full(circular_array) && !circular_array->dynamic) {
+        exit(printf("Error: circular array is full and not dynamic.\n"));
+    }
+    else if(circular_array_is_full(circular_array) && circular_array->dynamic) {
+        // Aumenta a capacidade do CircularArray
+        int new_capacity = circular_array->capacity * 2;
+        data_type* new_items = malloc(sizeof(data_type) * new_capacity);
+
+        // Copia os elementos do CircularArray atual para o novo array com a nova capacidade
+        int i, j;
+        for (i = circular_array->front, j = 0; j < circular_array->size; i = (i + 1) % circular_array->capacity, j++) {
+            new_items[j] = circular_array->items[i];
+        }
+
+        // Atualiza os ponteiros e capacidade do CircularArray
+        circular_array->front = 0;
+        circular_array->rear = circular_array->size - 1;
+        circular_array->capacity = new_capacity;
+
+        // Libera a memória do array antigo
+        free(circular_array->items);
+
+        // Atualiza o ponteiro de items para apontar para o novo array
+        circular_array->items = new_items;
+    }
+
+    // Avança o ponteiro rear e adiciona o novo elemento no CircularArray
+    circular_array->rear = (circular_array->rear + 1) % circular_array->capacity;
+    circular_array->items[circular_array->rear] = data;
+
+    // Incrementa o tamanho do CircularArray
+    circular_array->size++;
+}
